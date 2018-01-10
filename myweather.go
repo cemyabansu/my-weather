14e881acd6e95baab7eb6c36cd80d126245a6889
Cem Yabansu <cemyabansu@hotmail.com>
2018-01-10 23:12:53 +0300

    trying some parameters.

--- myweather.go
+++ myweather.go
@@ -25,15 +25,28 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "my-weather"
 	app.Usage = "get weather information from console."
+
+	var places string
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:"places, p",
+			Value: "istanbul",
+			Usage: "places to get weather info",
+			Destination: &places,
+		},
+	}
+
 	app.Action = func(c *cli.Context) error {
-		return handleWeatherAction()
+		fmt.Println(places)
+		return handleWeatherAction(places)
 	}
 
 	app.Run(os.Args)
 }
 
-func handleWeatherAction() error{
-	openWeatherUriTemplate := "http://api.openweathermap.org/data/2.5/weather?q=istanbul&units=metric&appid=e07b609b458eecd57dc0cad0fdb9aa9b"
+func handleWeatherAction(places string) error{
+	openWeatherUriTemplate := "http://api.openweathermap.org/data/2.5/weather?q="+ places +"&units=metric&appid=e07b609b458eecd57dc0cad0fdb9aa9b"
 
 	weatherClient := http.Client{
 		Timeout: time.Second * 10, // maximum 10 seconds to timeout
@@ -62,6 +75,11 @@ func handleWeatherAction() error{
 		log.Fatal(err)
 	}
 
-	fmt.Println(weatherResponse.Weather[0].Main + " - " + weatherResponse.Weather[0].Description)
+	for index := 0; index < len(weatherResponse.Weather); index++ {
+		fmt.Println(weatherResponse.Weather[index].Main + " - " + weatherResponse.Weather[0].Description)	
+	}
+
+	fmt.Println(openWeatherUriTemplate)
+
 	return nil
 }
