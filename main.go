00af4072073e94b305f579626bc2b6b762bc6fd3
Cem Yabansu <cemyabansu@hotmail.com>
2018-01-10 22:45:22 +0300

    urfave/cli added.

--- main.go
+++ main.go
@@ -2,25 +2,37 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
-	"time"
 	"net/http"
-	"fmt"
+	"os"
+	"time"
+
+	"github.com/urfave/cli"
 )
 
 type weatherResponse struct {
-	Weather []weather			`json:"weather"`
+	Weather []weather `json:"weather"`
 }
 
 type weather struct {
-	Main string					`json:"main"`
-	Description string			`json:"description"`
+	Main        string `json:"main"`
+	Description string `json:"description"`
 }
 
-func main(){
-	fmt.Println("started.")
+func main() {
+	app := cli.NewApp()
+	app.Name = "my-weather"
+	app.Usage = "get weather information from console."
+	app.Action = func(c *cli.Context) error {
+		return handleWeatherAction()
+	}
 
+	app.Run(os.Args)
+}
+
+func handleWeatherAction() error{
 	openWeatherUriTemplate := "http://api.openweathermap.org/data/2.5/weather?q=istanbul&units=metric&appid=e07b609b458eecd57dc0cad0fdb9aa9b"
 
 	weatherClient := http.Client{
@@ -28,27 +40,28 @@ func main(){
 	}
 
 	req, err := http.NewRequest(http.MethodGet, openWeatherUriTemplate, nil)
-	if(err != nil){
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	res, err := weatherClient.Do(req)
-	if(err != nil){
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println("res code " + res.Status)
 
 	body, err := ioutil.ReadAll(res.Body)
-    if err != nil {
-        log.Fatal(err)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	weatherResponse := weatherResponse{}
 	err = json.Unmarshal(body, &weatherResponse)
 	if err != nil {
-        log.Fatal(err)
+		log.Fatal(err)
 	}
 
 	fmt.Println(weatherResponse.Weather[0].Main + " - " + weatherResponse.Weather[0].Description)
+	return nil
 }
