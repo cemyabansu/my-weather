12e7e250d01dedd74d9bc1c338f23b5e6fa0639b
Cem Yabansu <cemyabansu@hotmail.com>
2018-01-14 22:26:51 +0300

    better info

--- myweather.go
+++ myweather.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
-	"net/http"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/urfave/cli"
@@ -14,13 +14,28 @@ import (
 )
 
 type weatherResponse struct {
-	TimeZoneOffset	int		`json:"offset"`
-	Currently		currently	`json:"currently"`
+	TimeZoneOffset int       `json:"offset"`
+	Currently      currently `json:"currently"`
+	Daily          daily     `json:"daily"`
 }
 
 type currently struct {
-	Summary			string		`json:"summary"`
-	Temperature		float64		`json:"temperature"`
+	Summary     string  `json:"summary"`
+	Temperature float64 `json:"temperature"`
+}
+
+type daily struct {
+	Summary string      `json:"summary"`
+	Data    []dailyData `json:"data"`
+}
+
+type dailyData struct {
+	Time              int     `json:"time"`
+	Summary           string  `json:"summary"`
+	TemperatureHigh   float64 `json:"temperatureHigh"`
+	TemperatureLow    float64 `json:"temperatureLow"`
+	PrecipType        string  `json:"precipType"`
+	PrecipProbability float64 `json:"precipProbability"`
 }
 
 func main() {
@@ -28,39 +43,40 @@ func main() {
 	app.Name = "my-weather"
 	app.Usage = "get weather information from console."
 
-	var places string
+	var place string
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:"places, p",
-			Value: "istanbul",
-			Usage: "places to get weather info",
-			Destination: &places,
+			Name:        "place, p",
+			Value:       "istanbul",
+			Usage:       "place to get weather info",
+			Destination: &place,
 		},
 	}
 
 	app.Action = func(c *cli.Context) error {
-		fmt.Println(places)
-		return handleWeatherAction(places)
+		fmt.Println(place)
+		return handleWeatherAction(place)
 	}
 
 	app.Run(os.Args)
 }
 
-func handleWeatherAction(places string) error{
+func handleWeatherAction(places string) error {
 	googleGeoLocationKey := "AIzaSyB_Nib5oe_wIKljESFzNLnPFx2SLa2fCHo"
 
-	c,err := maps.NewClient(maps.WithAPIKey(googleGeoLocationKey))
+	c, err := maps.NewClient(maps.WithAPIKey(googleGeoLocationKey))
 	if err != nil {
 		log.Fatalf("fatal error on creating map client. error : %s", err)
 	}
 	r := &maps.GeocodingRequest{
 		Address: places,
 	}
-	geoResult, err := c.Geocode(context.Background(),r)
+	geoResult, err := c.Geocode(context.Background(), r)
 	lat := geoResult[0].Geometry.Location.Lat
 	lng := geoResult[0].Geometry.Location.Lng
-	fmt.Printf("result : %v, %v \n", lat, lng)
+	fmt.Printf("result : %s --> %v, %v \n", geoResult[0].FormattedAddress, lat, lng)
+	fmt.Printf("Here is the weather for %s \n", geoResult[0].FormattedAddress)
 
 	weatherApiUrlTemplate := "https://api.darksky.net/forecast/%s/%f,%f?exclude=hourly&lang=%s&units=%s"
 	apiKey := "8783cc8f39ddbbd37ecc97ea0c958e0d"
@@ -74,8 +90,8 @@ func handleWeatherAction(places string) error{
 	if err != nil {
 		fmt.Printf("The HTTP request failed with error %s\n", err)
 		return err
-    } 
-	
+	}
+
 	data, _ := ioutil.ReadAll(response.Body)
 
 	weatherResponse := weatherResponse{}
@@ -85,7 +101,27 @@ func handleWeatherAction(places string) error{
 		return err
 	}
 
-	fmt.Println("summary :" + weatherResponse.Currently.Summary)
+	fmt.Printf("Currently \nSummary\t\t: %s\nTemperature\t: %f°C\n",
+		weatherResponse.Currently.Summary,
+		weatherResponse.Currently.Temperature)
+
+	fmt.Println("Here is weekly result.")
+	fmt.Printf("Summary: %s\n\n", weatherResponse.Daily.Summary)
+
+	counter := 0
+	for _, dailyData := range weatherResponse.Daily.Data {
+		fmt.Println(counter)
+
+		fmt.Printf("Summary: %s\nTemperature Low \t: %f°C\nTemperature High \t: %f°C\nPrecipType \t\t: %s\nPrecipProbability \t: %f\n",
+			dailyData.Summary,
+			dailyData.TemperatureLow,
+			dailyData.TemperatureHigh,
+			dailyData.PrecipType,
+			dailyData.PrecipProbability*100)
+		fmt.Println("==================")
+
+		counter++
+	}
 
 	return nil
 }
